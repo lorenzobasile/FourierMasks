df7c802eaeacdebe091778aa2e53ade7a8fc63b1
Lorenzo Basile <56440763+lorenzobasile@users.noreply.github.com>
2022-12-20 16:14:17 +0100

    attack test

--- utils.py
+++ utils.py
@@ -143,3 +143,61 @@ def singleInv(base_model, clean, x, y, n_epochs, lam, idx, path):
                 idx+=1
                 break
     return idx
+
+def singleAttack(base_model, clean, x, y, n_epochs, lam, idx, path):
+
+    loss=torch.nn.CrossEntropyLoss()
+    device=torch.device("cuda:0" if next(base_model.parameters()).is_cuda else "cpu")
+
+    n=len(x)
+    x=x.to(device)
+    y=y.to(device)
+    clean=clean.to(device)
+    base_out=base_model(clean)
+    losses=[[] for i in range(n)]
+    werecorrect=(np.where((torch.argmax(base_out, axis=1)==y).cpu())[0]) #only correctly classified images
+
+    for i in range(n):
+        model=MaskedClf(Mask().to(device), base_model)
+        for p in model.clf.parameters():
+            p.requires_grad=False
+        model.mask.train()
+        optimizer=torch.optim.Adam(model.mask.parameters(), lr=0.01)
+        epoch=0
+        while True:
+            print("Epoch: ", epoch)
+            out=model(clean[i])
+            invariance=loss(out, y[i].reshape(1))
+            penalty=(model.mask.weight-1.0).abs().sum()
+            print("Pen: ", penalty*lam)
+            print("Inv: ", invariance)
+            l=penalty*lam-invariance
+            losses[i].append(l.item())
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            model.mask.weight.data.clamp_(0.)
+            epoch+=1
+            if epoch>500 and abs(l.item()-np.mean(losses[i][-20:]))<1e-5:
+                correct=torch.argmax(out, axis=1)==y[i]
+                if correct and i in werecorrect:
+                    mask=np.fft.fftshift(model.mask.weight.detach().cpu().reshape(3,224,224))
+                    #plt.figure(figsize=(30,20))
+                    #plt.plot(losses[i])
+                    #plt.savefig(path+"figures/"+str(y[i].item())+"/"+str(idx)+"loss.png")
+                    plt.figure()
+                    plt.imshow(mask[0], cmap="Blues")
+                    plt.colorbar()
+                    plt.savefig(path+"figures/"+str(y[i].item())+"/"+str(idx)+"R.png")
+                    plt.figure()
+                    plt.imshow(mask[1], cmap="Blues")
+                    plt.colorbar()
+                    plt.savefig(path+"figures/"+str(y[i].item())+"/"+str(idx)+"G.png")
+                    plt.figure()
+                    plt.imshow(mask[2], cmap="Blues")
+                    plt.colorbar()
+                    plt.savefig(path+"figures/"+str(y[i].item())+"/"+str(idx)+"B.png")
+                    np.save(path+"masks/"+str(y[i].item())+"/"+str(idx)+".npy", mask)
+                idx+=1
+                break
+    return idx
